d19b4ee1f0c8dd350c0c8397ee373e718f3fedd3
zhiminzhang0830 <452516515@qq.com>
2025-11-03 12:03:06 +0000

    update repeat

--- paddle_geometric/utils/repeat.py
+++ paddle_geometric/utils/repeat.py
@@ -3,34 +3,25 @@ import numbers
 from typing import Any
 
 import paddle
-from paddle import Tensor
 
 
 def repeat(src: Any, length: int) -> Any:
     if src is None:
         return None
-
-    if isinstance(src, Tensor):
-        if src.numel() == 1:
-            return src.tile([length])
-
-        if src.numel() > length:
+    if isinstance(src, paddle.Tensor):
+        if src.size == 1:
+            return src.tile(repeat_times=length)
+        if src.size > length:
             return src[:length]
-
-        if src.numel() < length:
-            last_elem = src[-1].unsqueeze(0)
-            padding = last_elem.tile([length - src.numel()])
-            return paddle.concat([src, padding])
-
+        if src.size < length:
+            last_elem = src[-1].unsqueeze(axis=0)
+            padding = last_elem.tile(repeat_times=length - src.size)
+            return paddle.concat(x=[src, padding])
         return src
-
     if isinstance(src, numbers.Number):
         return list(itertools.repeat(src, length))
-
     if len(src) > length:
         return src[:length]
-
     if len(src) < length:
         return src + list(itertools.repeat(src[-1], length - len(src)))
-
     return src

746399667efa75d97ec0ced6586d8567ed760f9f
zhiminzhang0830 <452516515@qq.com>
2025-11-04 03:11:31 +0000

    update hetero

--- paddle_geometric/utils/hetero.py
+++ paddle_geometric/utils/hetero.py
@@ -1,8 +1,6 @@
 from typing import Dict, List, Optional, Set, Tuple, Union
 
 import paddle
-from paddle import Tensor
-from paddle.nn import LayerDict
 
 from paddle_geometric.typing import Adj, EdgeType, NodeType, SparseTensor
 from paddle_geometric.utils import is_sparse, to_edge_index
@@ -10,55 +8,45 @@ from paddle_geometric.utils.num_nodes import maybe_num_nodes_dict
 
 
 def group_hetero_graph(
-    edge_index_dict: Dict[EdgeType, Tensor],
+    edge_index_dict: Dict[EdgeType, paddle.Tensor],
     num_nodes_dict: Optional[Dict[NodeType, int]] = None,
 ) -> Tuple[
-        Tensor,
-        Tensor,
-        Tensor,
-        Tensor,
-        Dict[Union[str, int], Tensor],
+        paddle.Tensor,
+        paddle.Tensor,
+        paddle.Tensor,
+        paddle.Tensor,
+        Dict[Union[str, int], paddle.Tensor],
         Dict[Union[NodeType, EdgeType], int],
 ]:
     num_nodes_dict = maybe_num_nodes_dict(edge_index_dict, num_nodes_dict)
-
     tmp = list(edge_index_dict.values())[0]
-
     key2int: Dict[Union[NodeType, EdgeType], int] = {}
-
-    cumsum, offset = 0, {}  # Helper data.
+    cumsum, offset = 0, {}
     node_types, local_node_indices = [], []
-    local2global: Dict[Union[str, int], Tensor] = {}
+    local2global: Dict[Union[str, int], paddle.Tensor] = {}
     for i, (key, N) in enumerate(num_nodes_dict.items()):
         key2int[key] = i
-        node_types.append(paddle.full((N,), i, dtype=tmp.dtype))
-        local_node_indices.append(paddle.arange(N, dtype=tmp.dtype))
+        node_types.append(
+            paddle.full(shape=(N, ), fill_value=i, dtype=tmp.dtype))
+        local_node_indices.append(paddle.arange(end=N))
         offset[key] = cumsum
         local2global[key] = local_node_indices[-1] + cumsum
         local2global[i] = local2global[key]
         cumsum += N
-
-    node_type = paddle.concat(node_types, axis=0)
-    local_node_idx = paddle.concat(local_node_indices, axis=0)
-
+    node_type = paddle.concat(x=node_types, axis=0)
+    local_node_idx = paddle.concat(x=local_node_indices, axis=0)
     edge_indices, edge_types = [], []
     for i, (keys, edge_index) in enumerate(edge_index_dict.items()):
         key2int[keys] = i
-        inc = paddle.to_tensor([offset[keys[0]], offset[keys[-1]]], dtype=tmp.dtype).reshape([2, 1])
-        edge_indices.append(edge_index + inc)
-        edge_types.append(paddle.full((edge_index.shape[1],), i, dtype=tmp.dtype))
-
-    edge_index = paddle.concat(edge_indices, axis=-1)
-    edge_type = paddle.concat(edge_types, axis=0)
-
-    return (
-        edge_index,
-        edge_type,
-        node_type,
-        local_node_idx,
-        local2global,
-        key2int,
-    )
+        inc = paddle.tensor([offset[keys[0]], offset[keys[-1]]]).view(2, 1)
+        edge_indices.append(edge_index + inc.to(tmp.place))
+        edge_types.append(
+            paddle.full(shape=(edge_index.shape[1], ), fill_value=i,
+                        dtype=tmp.dtype))
+    edge_index = paddle.concat(x=edge_indices, axis=-1)
+    edge_type = paddle.concat(x=edge_types, axis=0)
+    return (edge_index, edge_type, node_type, local_node_idx, local2global,
+            key2int)
 
 
 def get_unused_node_types(node_types: List[NodeType],
@@ -67,62 +55,70 @@ def get_unused_node_types(node_types: List[NodeType],
     return set(node_types) - set(dst_node_types)
 
 
-def check_add_self_loops(
-    module: paddle.nn.Layer,
-    edge_types: List[EdgeType],
-) -> None:
-    is_bipartite = any([key[0] != key[-1] for key in edge_types])
-    if is_bipartite and getattr(module, 'add_self_loops', False):
+def check_add_self_loops(module: paddle.nn.Layer,
+                         edge_types: List[EdgeType]) -> None:
+    is_bipartite = any([(key[0] != key[-1]) for key in edge_types])
+    if is_bipartite and getattr(module, "add_self_loops", False):
         raise ValueError(
-            f"'add_self_loops' attribute set to 'True' on module '{module}' "
-            f"for use with edge type(s) '{edge_types}'. This will lead to "
-            f"incorrect message passing results.")
+            f"'add_self_loops' attribute set to 'True' on module '{module}' for "
+            f"use with edge type(s) '{edge_types}'. This will lead to incorrect "
+            "message passing results.")
 
 
 def construct_bipartite_edge_index(
     edge_index_dict: Dict[EdgeType, Adj],
     src_offset_dict: Dict[EdgeType, int],
     dst_offset_dict: Dict[NodeType, int],
-    edge_attr_dict: Optional[Dict[EdgeType, Tensor]] = None,
+    edge_attr_dict: Optional[Dict[EdgeType, paddle.Tensor]] = None,
     num_nodes: Optional[int] = None,
-) -> Tuple[Adj, Optional[Tensor]]:
+) -> Tuple[Adj, Optional[paddle.Tensor]]:
     """Constructs a tensor of edge indices by concatenating edge indices
     for each edge type. The edge indices are increased by the offset of the
     source and destination nodes.
+
+    Args:
+        edge_index_dict (Dict[Tuple[str, str, str], torch.Tensor]): A
+            dictionary holding graph connectivity information for each
+            individual edge type, either as a :class:`torch.Tensor` of
+            shape :obj:`[2, num_edges]` or a
+            :class:`torch_sparse.SparseTensor`.
+        src_offset_dict (Dict[Tuple[str, str, str], int]): A dictionary of
+            offsets to apply to the source node type for each edge type.
+        dst_offset_dict (Dict[str, int]): A dictionary of offsets to apply for
+            destination node types.
+        edge_attr_dict (Dict[Tuple[str, str, str], torch.Tensor]): A
+            dictionary holding edge features for each individual edge type.
+            (default: :obj:`None`)
+        num_nodes (int, optional): The final number of nodes in the bipartite
+            adjacency matrix. (default: :obj:`None`)
     """
     is_sparse_tensor = False
-    edge_indices: List[Tensor] = []
-    edge_attrs: List[Tensor] = []
+    edge_indices: List[paddle.Tensor] = []
+    edge_attrs: List[paddle.Tensor] = []
     for edge_type, src_offset in src_offset_dict.items():
         edge_index = edge_index_dict[edge_type]
         dst_offset = dst_offset_dict[edge_type[-1]]
-
         is_sparse_tensor = isinstance(edge_index, SparseTensor)
         if is_sparse(edge_index):
             edge_index, _ = to_edge_index(edge_index)
-            edge_index = paddle.flip(edge_index, [0])
+            edge_index = edge_index.flip(axis=[0])
         else:
             edge_index = edge_index.clone()
-
         edge_index[0] += src_offset
         edge_index[1] += dst_offset
         edge_indices.append(edge_index)
-
         if edge_attr_dict is not None:
-            if isinstance(edge_attr_dict, LayerDict):
-                value = edge_attr_dict['__'.join(edge_type)]
+            if isinstance(edge_attr_dict, paddle.nn.ParameterDict):
+                value = edge_attr_dict["__".join(edge_type)]
             else:
                 value = edge_attr_dict[edge_type]
             if value.shape[0] != edge_index.shape[1]:
-                value = paddle.expand(value, shape=[edge_index.shape[1], -1])
+                value = value.expand(shape=[edge_index.shape[1], -1])
             edge_attrs.append(value)
-
-    edge_index = paddle.concat(edge_indices, axis=1)
-
-    edge_attr: Optional[Tensor] = None
+    edge_index = paddle.concat(x=edge_indices, axis=1)
+    edge_attr: Optional[paddle.Tensor] = None
     if edge_attr_dict is not None:
-        edge_attr = paddle.concat(edge_attrs, axis=0)
-
+        edge_attr = paddle.concat(x=edge_attrs, axis=0)
     if is_sparse_tensor:
         edge_index = SparseTensor(
             row=edge_index[1],
@@ -130,5 +126,4 @@ def construct_bipartite_edge_index(
             value=edge_attr,
             sparse_sizes=(num_nodes, num_nodes),
         )
-
     return edge_index, edge_attr
---
+++ test/utils/test_hetero.py
@@ -0,0 +1,40 @@
+import paddle
+
+from paddle_geometric.testing import get_random_edge_index
+from paddle_geometric.utils.hetero import construct_bipartite_edge_index
+
+
+def test_construct_bipartite_edge_index():
+    edge_index = get_random_edge_index(4, 6, num_edges=20)
+
+    edge_index_dict = {
+        ('author', 'paper'): edge_index,
+        ('paper', 'author'): edge_index.flip([0]),
+    }
+    edge_attr_dict = {
+        ('author', 'paper'): paddle.randn(edge_index.size(1), 16),
+        ('paper', 'author'): paddle.randn(edge_index.size(1), 16)
+    }
+
+    edge_index, edge_attr = construct_bipartite_edge_index(
+        edge_index_dict,
+        src_offset_dict={
+            ('author', 'paper'): 0,
+            ('paper', 'author'): 4
+        },
+        dst_offset_dict={
+            'author': 0,
+            'paper': 4
+        },
+        edge_attr_dict=edge_attr_dict,
+    )
+
+    assert edge_index.size() == (2, 40)
+    assert edge_index.min() >= 0
+    assert edge_index[0].max() > 4 and edge_index[1].max() > 6
+    assert edge_index.max() <= 10
+    assert edge_attr.size() == (40, 16)
+    assert paddle.equal_all(edge_attr[:20], edge_attr_dict['author',
+                                                           'paper']).item()
+    assert paddle.equal_all(edge_attr[20:], edge_attr_dict['paper',
+                                                           'author']).item()
